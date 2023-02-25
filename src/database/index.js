023844f6e4dab183f9157e17b94d3c17d9ab1b8e
octavio-io <octavioveiga13@gmail.com>
2023-02-25 19:09:59 -0300

    fix issues

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,10 @@
-import process from 'dotenv'
 import mongoose from 'mongoose'
+import process from 'dotenv'
 
 const { parsed } = process.config()
 
-mongoose.set('strictQuery', false)
-mongoose.Promise = global.Promise
+//mongoose.set('strictQuery', false)
+//mongoose.Promise = global.Promise
 mongoose.connect(parsed.MONGODB_URI)
 
 export default mongoose

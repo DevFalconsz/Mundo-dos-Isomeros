753d245928c5815bd689084fca6d222d1513586d
octavio-io <octavioveiga13@gmail.com>
2023-02-26 01:24:36 -0300

    fix static

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 //import bodyParser from 'body-parser'
 import { createServer } from 'http'
 import express from 'express'
+import path from 'node:path'
 
 const app = express()
 const server = createServer(app)
@@ -9,7 +10,7 @@ const server = createServer(app)
 //app.use(bodyParser.json())
 //app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use("/", express.static("public"))
+app.use("/", express.static(path.join(path.dirname("public"), "public")))
 //app.use("/levels", express.static("public/levels"))
 //app.use("/auth", router)
 
